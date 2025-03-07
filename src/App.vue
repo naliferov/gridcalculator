2a4a6effefc9a6c4d8dc225374265cf3a62003ae
Nick Aliferov <x8core@gmail.com>
2025-03-07 23:19:54 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+/* @media (min-width: 1024px) {
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
+
+</style>
