91bab444b2cdd912da024bed1ed646f550980bff
Nick Aliferov <x8core@gmail.com>
2025-03-08 21:53:23 +0200

    update layout for grid inputs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header></header>
-
   <main>
     <TheWelcome />
   </main>
