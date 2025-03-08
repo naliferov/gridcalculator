a8c5d67a6ac470c7e353ee8b4098b33130d8b64a
Nick Aliferov <x8core@gmail.com>
2025-03-08 22:13:38 +0200

    restore file with layout

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,71 @@
+<script setup>
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+</script>
+
+<template>
+
+<div class="grid grid-cols-3 grid-rows-2 gap-4">
+    <!-- Первая строка -->
+    <div class="flex">
+      <Label for="input1">Max width:</Label>
+      <Input id="input1" type="text" />
+    </div>
+    <div class="flex">
+      <Label for="input2">Gutter width:</Label>
+      <Input id="input2" type="text" />
+    </div>
+    <div class="flex">
+      <Label for="input3">Page width: </Label>
+      <Input id="input3" type="text" />
+    </div>
+    
+    <div class="flex">
+      <Label for="input4">Input 4: </Label>
+      <Input id="input4" type="text" />
+    </div>
+    <div class="flex">
+      <Label for="input5">Input 5: </Label>
+      <Input id="input5" type="text" />
+    </div>
+    <div class="flex">
+      <Label for="input6">Input 6: </Label>
+      <Input id="input6" type="text" />
+    </div>
+</div>
+
+  
+    
+</template>
+
+<style scoped>
+.item {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+.details {
+  flex: 1;
+  margin-left: 1rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 32px;
+  height: 32px;
+
+  color: var(--color-text);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+</style>
