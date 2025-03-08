24a0343ef18098515680392eca80a338a76cfb6d
Nick Aliferov <x8core@gmail.com>
2025-03-08 22:13:10 +0200

    restore file with layout

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,4 @@ header {
   line-height: 1.5;
 }
 
-/* @media (min-width: 1024px) {
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
-
 </style>
